631337c6deb654b553ecd98d519074f1671e2c7b
nimbus117 <mail@nimbus117.co.uk>
2018-09-11 17:49:00 +0100

    added renderFamilyInstruments method and called from within the Instruments:selected-ready subscriber callback

--- src/views/result_view.js
+++ src/views/result_view.js
@@ -7,18 +7,33 @@ const ResultView = function (section) {
 ResultView.prototype.bindEvents = function () {
   PubSub.subscribe('Instruments:selected-ready', e => {
     console.log(e.detail);
+    this.section.innerHTML = '';
     this.renderFamilyInfo(e.detail);
+    this.renderFamilyInstruments(e.detail);
   })
 }
 
 ResultView.prototype.renderFamilyInfo = function (family) {
   const familyInfoHeading = document.createElement('h2');
   familyInfoHeading.textContent = family.name;
+  this.section.appendChild(familyInfoHeading);
   const familyInfoParagraph = document.createElement('p');
   familyInfoParagraph.textContent = family.description;
-  this.section.innerHTML = '';
-  this.section.appendChild(familyInfoHeading);
   this.section.appendChild(familyInfoParagraph);
+}
+
+ResultView.prototype.renderFamilyInstruments = function (family) {
+  const familyInstrumentHeading = document.createElement('h2');
+  familyInstrumentHeading.textContent = 'Instruments Include';
+  this.section.appendChild(familyInstrumentHeading);
+  const familyInstrumentList = document.createElement('ul');
+  this.section.appendChild(familyInstrumentList);
+  family.instruments.forEach(instr => {
+    console.log(instr);
+    let listItem = document.createElement('li');
+    listItem.textContent = instr;
+    familyInstrumentList.appendChild(listItem);
+  })
 }
 
 module.exports = ResultView;
