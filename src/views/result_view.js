6c79223f5f17d6a5cb72501e9a3a01b54d87c7fd
nimbus117 <mail@nimbus117.co.uk>
2018-09-11 17:39:10 +0100

    added renderFamilyInfo method and called from within the Instruments:selected-ready subscriber callback

--- src/views/result_view.js
+++ src/views/result_view.js
@@ -7,7 +7,18 @@ const ResultView = function (section) {
 ResultView.prototype.bindEvents = function () {
   PubSub.subscribe('Instruments:selected-ready', e => {
     console.log(e.detail);
+    this.renderFamilyInfo(e.detail);
   })
+}
+
+ResultView.prototype.renderFamilyInfo = function (family) {
+  const familyInfoHeading = document.createElement('h2');
+  familyInfoHeading.textContent = family.name;
+  const familyInfoParagraph = document.createElement('p');
+  familyInfoParagraph.textContent = family.description;
+  this.section.innerHTML = '';
+  this.section.appendChild(familyInfoHeading);
+  this.section.appendChild(familyInfoParagraph);
 }
 
 module.exports = ResultView;
